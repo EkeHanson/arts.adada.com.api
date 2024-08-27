7b0bf18aa57a77325804dc6f41c6214e42214bf0
Abraham Ekene-onwon Hanson <ekenehanson@gmail.com>
2024-08-27 10:52:16 +0100

    Updated the project url to include media files

--- new_arts/urls.py
+++ new_arts/urls.py
@@ -1,6 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib import admin
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
+
 
 
 
@@ -9,4 +15,13 @@ urlpatterns = [
     
     path('api/courses/', include('courses.urls')),
     path('api/register/', include('register.urls')),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
